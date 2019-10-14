ba2120e35cbf8f2da1ff88d2185862a66234a7e8
veilnui <account@veilnui.net>
2019-10-14 23:48:43 +0900

    skin control

---
+++ skins/lime/functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Skin control
+ */
+global $_THEME_OPTIONS;
+$_THEME_OPTIONS = [
+  // All
+  'site_key_color' => '',
+  'site_key_text_color' => '',
+  'site_text_color' => '',
+  'site_background_color' => '',
+  'site_link_color' => '',
+  'site_selection_color' => '',
+  'site_selection_background_color' => '',
+  'sidebar_position' => 'sidebar_right',
+  // header
+  'header_container_background_color' => '',
+  'header_container_text_color' => '',
+  'header_background_color' => '',
+  'header_text_color' => '',
+  'global_navi_background_color' => '',
+  'global_navi_text_color' => '',
+];
