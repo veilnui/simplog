b8058d8308c2f057c510e64402918926606eb93c
veilnui <54118059+veilnui@users.noreply.github.com>
2019-12-02 23:37:35 +0900

    スキン制御

--- skins/cyan/functions.php
+++ skins/cyan/functions.php
@@ -1,32 +1,30 @@
 <?php
 /**
  * Skin control
+ * スキン制御
  */
 global $_THEME_OPTIONS;
 $_THEME_OPTIONS = [
-  // all
+  // all 全体
   'site_key_color' => '',
   'site_key_text_color' => '',
   'site_text_color' => '',
   'site_background_color' => '',
-  'site_link_color' => '',
-  'site_selection_color' => '',
-  'site_selection_background_color' => '',
   'sidebar_position' => 'sidebar_right',
-  // header
+  // header ヘッダー
   'header_container_background_color' => '',
   'header_container_text_color' => '',
   'header_background_color' => '',
   'header_text_color' => '',
   'global_navi_background_color' => '',
   'global_navi_text_color' => '',
-  // column
+  // column カラム
   'main_column_border_color' => '',
   'sidebar_border_color' => '',
-  // footer
+  // footer フッター
   'footer_background_color' => '',
   'footer_text_color' => '',
-  // go to top
+  // go to top Go to topボタン
   'go_to_top_background_color' => '',
   'go_to_top_text_color' => '',
 ];
